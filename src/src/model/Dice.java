012f3d29fac56db1785796949be5c7730cf4c13d
JasperMooren <jrhmoore@avans.nl>
2018-04-19 12:35:19 +0200

    Various methods added
    Setup, turn, and distributeResources at the Catan side added. Most of these methods still go towards some empty methods in other classes.

--- src/src/model/Dice.java
+++ src/src/model/Dice.java
@@ -2,4 +2,9 @@ package model;
 
 public class Dice {
 
+	public int roll() {
+		// TODO Auto-generated method stub
+		return 0; //DEBUG shouldn't be 0, just to fix the error
+	}
+
 }
--- src/src/model/Gameboard.java
+++ src/src/model/Gameboard.java
@@ -1,5 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Gameboard {
+
+	//Outer array is the players, the Inner ArrayList is the actual resources the player gets. 
+	public ArrayList<Resource>[] distributeResources(int roll) {
+		//ArrayList<Resource>[] is an array of players (length 4) which contains an ArrayList of Resources
+		return null; //TODO actual return value, this is just to fix the error. 
+		// TODO Auto-generated method stub
+		
+	}
 
 }
