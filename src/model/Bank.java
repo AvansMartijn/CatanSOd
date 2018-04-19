f9d7faad25f74898a47b021a63eb71a6e110566d
Jim <38460550+jjmhalew@users.noreply.github.com>
2018-04-19 11:10:21 +0200

    Finished Bank... for now

--- src/model/Bank.java
+++ src/model/Bank.java
@@ -10,40 +10,47 @@ public class Bank {
 	private final static int AMOUNT_OF_MONOPOLY = 2;
 	private final static int AMOUNT_OF_ROADBUILDING = 2;
 	private final static int AMOUNT_OF_YEAROFPLENTY = 2;
-	private final static int AMOUNT_OF_VICTORYPOINT = 5;	
-	
+	private final static int AMOUNT_OF_VICTORYPOINT = 5;
+
 	// Instance variables
 	private ArrayList<ArrayList<Resource>> resources;
 	private ArrayList<DevelopmentCard> developmentCards;
-	
+
 	// Constructor
 	public Bank() {
 		resources = new ArrayList<ArrayList<Resource>>();
-		
+		createResourceCards();
+		developmentCards = new ArrayList<DevelopmentCard>();
+		createDevelopmentCards();
+	}
+
+	// Add Resources to resources ArrayList
+	private void createResourceCards() {
+
 		// For the amount of different resources
-		for(int i = 0; i < AMOUNT_OF_DIFFERENT_RESOURCES; i++) {
+		for (int i = 0; i < AMOUNT_OF_DIFFERENT_RESOURCES; i++) {
 			resources.add(new ArrayList<Resource>());
 			// For the amount needed of each resource
-			for(int j = 0; j < AMOUNT_OF_EACH_RESOURCE; j++) {
-				ResourceType[] resourceTypes = ResourceType.values(); 
+			for (int j = 0; j < AMOUNT_OF_EACH_RESOURCE; j++) {
+				ResourceType[] resourceTypes = ResourceType.values();
 				resources.get(i).add(new Resource(resourceTypes[i]));
 			}
 		}
-		developmentCards = new ArrayList<DevelopmentCard>();
-		create();
 	}
-	
-	// Create
-	private void create() {
-		
+
+	// Add DevelopmentCards to developmentCards ArrayList
+	private void createDevelopmentCards() {
+
 		// Arrays
-		DevelopmentCard[] typeCard = new DevelopmentCard[] {new Knight(), new Monopoly(), new RoadBuilding(), new YearOfPlenty(), new VictoryPoint()};
-		int amountOfCards[] = new int[] {AMOUNT_OF_KNIGHTS, AMOUNT_OF_MONOPOLY, AMOUNT_OF_ROADBUILDING, AMOUNT_OF_YEAROFPLENTY, AMOUNT_OF_VICTORYPOINT};
+		DevelopmentCard[] typeCard = new DevelopmentCard[] { new Knight(), new Monopoly(), new RoadBuilding(),
+				new YearOfPlenty(), new VictoryPoint() };
+		int amountOfCards[] = new int[] { AMOUNT_OF_KNIGHTS, AMOUNT_OF_MONOPOLY, AMOUNT_OF_ROADBUILDING,
+				AMOUNT_OF_YEAROFPLENTY, AMOUNT_OF_VICTORYPOINT };
 
 		// for the amount of typeCards
-		for(int i = 0; i < typeCard.length; i++) {
+		for (int i = 0; i < typeCard.length; i++) {
 			// for the amount of cards needed of a typeCard
-			for(int j = 0; j < amountOfCards[i]; j++) {
+			for (int j = 0; j < amountOfCards[i]; j++) {
 				developmentCards.add(typeCard[i]);
 			}
 		}
