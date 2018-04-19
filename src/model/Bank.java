2a6cc6b76788f4c36670c2eab55c9e828d781c82
Jim <38460550+jjmhalew@users.noreply.github.com>
2018-04-19 10:46:31 +0200

    Bank
    Completed DevelopmentCard create function

--- src/model/Bank.java
+++ src/model/Bank.java
@@ -7,7 +7,9 @@ public class Bank {
 	private final static int AMOUNT_OF_DIFFERENT_RESOURCES = 5;
 	private final static int AMOUNT_OF_EACH_RESOURCE = 19;
 	private final static int AMOUNT_OF_KNIGHTS = 14;
-	private final static int AMOUNT_OF_MONOPOLY = 6;
+	private final static int AMOUNT_OF_MONOPOLY = 2;
+	private final static int AMOUNT_OF_ROADBUILDING = 2;
+	private final static int AMOUNT_OF_YEAROFPLENTY = 2;
 	private final static int AMOUNT_OF_VICTORYPOINT = 5;	
 	
 	// Instance variables
@@ -21,30 +23,29 @@ public class Bank {
 		// For the amount of different resources
 		for(int i = 0; i < AMOUNT_OF_DIFFERENT_RESOURCES; i++) {
 			resources.add(new ArrayList<Resource>());
-			
+			// For the amount needed of each resource
 			for(int j = 0; j < AMOUNT_OF_EACH_RESOURCE; j++) {
 				ResourceType[] resourceTypes = ResourceType.values(); 
 				resources.get(i).add(new Resource(resourceTypes[i]));
 			}
 		}
-		
 		developmentCards = new ArrayList<DevelopmentCard>();
+		create();
 	}
 	
 	// Create
 	private void create() {
 		
-		// Add Knights to developmentCards
-		for(int i = 0; i < AMOUNT_OF_KNIGHTS; i++) {
-			resources.add(Knight);
-		}
-		// Add Monopoly to developmentCards
-		for(int i = 0; i < AMOUNT_OF_MONOPOLY; i++) {
-			resources.add(Monopoly);
-		}
-		// Add VictoryPoint to developmentCards
-		for(int i = 0; i < AMOUNT_OF_VICTORYPOINT; i++) {
-			resources.add(VictoryPoint);
+		// Arrays
+		DevelopmentCard[] typeCard = new DevelopmentCard[] {new Knight(), new Monopoly(), new RoadBuilding(), new YearOfPlenty(), new VictoryPoint()};
+		int amountOfCards[] = new int[] {AMOUNT_OF_KNIGHTS, AMOUNT_OF_MONOPOLY, AMOUNT_OF_ROADBUILDING, AMOUNT_OF_YEAROFPLENTY, AMOUNT_OF_VICTORYPOINT};
+
+		// for the amount of typeCards
+		for(int i = 0; i < typeCard.length; i++) {
+			// for the amount of cards needed of a typeCard
+			for(int j = 0; j < amountOfCards[i]; j++) {
+				developmentCards.add(typeCard[i]);
+			}
 		}
 	}
 }
---
+++ src/model/RoadBuilding.java
@@ -0,0 +1,5 @@
+package model;
+
+public class RoadBuilding extends DevelopmentCard {
+
+}
