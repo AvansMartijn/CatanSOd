cd937c20a1fe2f6774675935a2f68a6e3f499eb2
AvansChiel <cgmarts@avans.nl>
2018-04-24 11:32:33 +0200

    changes
    added robber, modified gameboard

---
+++ src/model/Robber.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Robber {
+	private Tile onTile;
+
+	public Tile getOnTile() {
+		return onTile;
+	}
+
+	public void setOnTile(Tile onTile) {
+		this.onTile = onTile;
+	}
+	
+}
