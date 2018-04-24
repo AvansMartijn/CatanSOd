a1bb140d189eb266f606f09c7f164b6a08eb29d4
avansruben <rgjrijen@avans.nl>
2018-04-24 11:07:16 +0200

    robber gemaakt
    robber gamaakt

---
+++ src/model/Robber.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.Vector;
+
+public class Robber {
+
+	Tile tile;
+	Vector victims;
+	Robber robber;
+	public Robber() {
+		tile = new Tile();
+		victims = null;
+	}
+
+	public void setresourceType() {
+		tile.setResourceType(null);
+	}
+
+	public Vector getVictims() {
+		return victims;
+	}
+
+	public void setVictims(Vector victims) {
+		this.victims = victims;
+	}
+
+	public Robber getRobber() {
+		return robber;
+	}
+
+	public void setRobber(Robber robber) {
+		this.robber = robber;
+	}
+
+
+
+}
---
+++ src/model/Tile.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Tile {
+
+	ResourceType resourceType;
+
+	public ResourceType getResourceType() {
+		return resourceType;
+	}
+
+	public void setResourceType(ResourceType resourceType) {
+		this.resourceType = resourceType;
+	}
+
+}
