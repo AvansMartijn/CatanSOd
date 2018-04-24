67dccbc002f56587920dfba4456864f528fa0b41
AvansChiel <cgmarts@avans.nl>
2018-04-24 10:43:44 +0200

    changes
    added tile, harbour. Modified buildingLocations to match database

--- src/model/BuildingLocation.java
+++ src/model/BuildingLocation.java
@@ -1,33 +1,29 @@
 package model;
 
 public class BuildingLocation {
-
-	private ResourceType resourceType;
-	private int id;
-	private int chipNumber;
+	private int x;
+	private int y;	
+	private Harbour harbour;
 	
-	public ResourceType getResourceType() {
-		return resourceType;
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public Harbour getHarbour() {
+		return harbour;
+	}
+	public void setHarbour(Harbour harbour) {
+		this.harbour = harbour;
 	}
 
-	public void setResourceType(ResourceType resourceType) {
-		this.resourceType = resourceType;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getChipNumber() {
-		return chipNumber;
-	}
-
-	public void setChipNumber(int chipNumber) {
-		this.chipNumber = chipNumber;
-	}
-
+	
 }
---
+++ src/model/Harbour.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Harbour {
+	private boolean hasHabour;
+	private ResourceType rsType;
+	
+	
+	public boolean isHasHabour() {
+		return hasHabour;
+	}
+	public void setHasHabour(boolean hasHabour) {
+		this.hasHabour = hasHabour;
+	}
+	public ResourceType getRsType() {
+		return rsType;
+	}
+	public void setRsType(ResourceType rsType) {
+		this.rsType = rsType;
+	}
+	
+	
+}
