0e76fc874626763ca44764d47423925addff64ae
AvansChiel <38489385+AvansChiel@users.noreply.github.com>
2018-04-25 11:30:13 +0200

    changes
    added tiles, buildinglocs to generate

--- src/model/BuildingLocation.java
+++ src/model/BuildingLocation.java
@@ -1,15 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+
 public class BuildingLocation {
 	private int x;
 	private int y;	
 	private Harbour harbour;
+	private ArrayList<Tile> tileArr;
 	
 	
-	public BuildingLocation(int x, int y, Harbour harbour) {
+	public BuildingLocation(int x, int y, Tile tile1, Tile tile2, Tile tile3, Harbour harbour) {
 		this.x = x;
 		this.y = y;
 		this.harbour = harbour;
+		if(tile1 != null) {
+			tileArr.add(tile1);
+		}
+		if(tile2 != null) {
+			tileArr.add(tile2);
+		}
+		if(tile3 != null) {
+			tileArr.add(tile3);
+		}
 	}
 	public int getX() {
 		return x;
