8f015c960ed549f4533de8f192124132ac11af43
AvansMartijn <mfghaneg@avans.nl>
2018-05-10 19:02:03 +0200

    Refactor Fix

--- src/view/GameGUIPanel.java
+++ src/view/GameGUIPanel.java
@@ -21,7 +21,7 @@ public class GameGUIPanel extends JPanel {
 	private Color myBackGroundColor = new Color(240, 226, 223);
 	
 	private GameTopPanel gameTopPanel;
-	private PlayerActionsPanel playerActionsPanel;
+	private PlayerActionPanel playerActionsPanel;
 	private MyResourcesPanel resourcesPanel;
 	private MyDevelopmentCardsPanel developmentCardsPanel;
 	private BoardPanel boardPanel;
@@ -40,7 +40,7 @@ public class GameGUIPanel extends JPanel {
 		 setPreferredSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
 		 */
 		gameTopPanel = new GameTopPanel("CatanTest"); // TODO set gamelabel text in constructor
-		playerActionsPanel = new PlayerActionsPanel();
+		playerActionsPanel = new PlayerActionPanel();
 		resourcesPanel = new MyResourcesPanel();
 		developmentCardsPanel = new MyDevelopmentCardsPanel();
 		boardPanel = new BoardPanel();
