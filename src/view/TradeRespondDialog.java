2d282113c32b6726f3de104aa3cc1383146cf762
Bauke Roefs <broefs1@avans.nl>
2018-05-23 17:35:51 +0200

    added class

--- src/view/TradeRespondDialog.java
+++ src/view/TradeRespondDialog.java
@@ -5,10 +5,18 @@ import javax.swing.JDialog;
 @SuppressWarnings("serial")
 public class TradeRespondDialog extends JDialog {
 
+	private TradeRespondPanels tradeRespondPanels;
+
 	public TradeRespondDialog() {
 
-		
-		
+		tradeRespondPanels = new TradeRespondPanels();
+
+		setContentPane(tradeRespondPanels);
+		pack();
+		setLocationRelativeTo(null);
+
+		setVisible(true);
+
 	}
 
 }
